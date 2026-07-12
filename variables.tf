525b834a33a5686a0d9f5d5dd41727b22e5d2f76
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:57:39 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -10,19 +10,23 @@ Optional:
     - secret
     - tags
     - value
+    - value_key_vault_id (alternative to value - read from Key Vault instead)
+    - value_key_vault_secret_name (alternative to value - read from Key Vault instead)
     - value_from_key_vault (block):
         - identity_client_id (optional)
         - secret_id (required)
 EOT
 
   type = map(object({
-    api_management_name = string
-    display_name        = string
-    name                = string
-    resource_group_name = string
-    secret              = optional(bool) # Default: false
-    tags                = optional(list(string))
-    value               = optional(string)
+    api_management_name         = string
+    display_name                = string
+    name                        = string
+    resource_group_name         = string
+    secret                      = optional(bool) # Default: false
+    tags                        = optional(list(string))
+    value                       = optional(string)
+    value_key_vault_id          = optional(string)
+    value_key_vault_secret_name = optional(string)
     value_from_key_vault = optional(object({
       identity_client_id = optional(string)
       secret_id          = string
